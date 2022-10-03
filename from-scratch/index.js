c3c1fcd785512ed1222157e8f133abfc77c7e642
Lenin Hasda <leninhasda@gmail.com>
2022-10-03 20:33:28 +0200

    Add a scratch project

---
+++ from-scratch/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const element = React.createElement(
+    'div',
+    null,
+    'React element'
+)
+
+ReactDOM.render(element, document.getElementById('root'));
